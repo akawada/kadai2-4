68169ebe347d7c1f95d60f4e9b667f5fd43e71c9
akawada <atsushicco51@hotmail.com>
2016-10-10 13:07:59 +0900

    pullreq

--- kadai2_4.java
+++ kadai2_4.java
@@ -4,34 +4,34 @@ public class kadai2_4 {
 	public static void main( String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String strin;
-		Integer num=0;
+		Integer num = 0;
 		// 入力の繰り返し
 
 		while (true) {
-			System.out.println("1~100の数値を入力してください。");				
+			System.out.println("1~100の数値を入力してください。");
 			strin = scan.next();
 			// 数値チェック
 			if (chknum(strin) == false){
-				System.err.println("不正な数値です。もう一度入力してください。");				
+				System.err.println("不正な数値です。もう一度入力してください。");
 			}
 			else{
 				num = Integer.parseInt(strin);
 				for (int i = 1; i <= num; i++){
 					
-					if(i%15==0){
 					//3と5の倍数
+					if(i%15 == 0){
 						System.out.println("FizzBuzz");
 					}
-					else if(i%5==0){
 					//5の倍数
+					else if(i%5 == 0){
 						System.out.println("Fizz");
 					}
-					else if(i%3==0){
 					//3の倍数
+					else if(i%3 == 0){
 						System.out.println("Buzz");
 					}
-					else{
 					//数字
+					else{
 						System.out.println(i);
 					}
 				}
