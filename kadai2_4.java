a7ac8869cd988a4972fd8bf7459714debc9b462b
akawada <atsushicco51@hotmail.com>
2016-10-15 03:09:41 +0900

    kadai2-4-new

--- kadai2_4.java
+++ kadai2_4.java
@@ -8,38 +8,37 @@ public class kadai2_4 {
 		// 入力の繰り返し
 
 		while (true) {
-			System.out.println("1~100の数値を入力してください。");
+			System.out.println("1~100の数値を入力してください。");				
 			strin = scan.next();
 			// 数値チェック
 			if (chknum(strin) == false){
-				System.err.println("不正な数値です。もう一度入力してください。");
+				System.err.println("不正な数値です。もう一度入力してください。");				
 			}
 			else{
 				num = Integer.parseInt(strin);
-				for (int i = 1; i <= num; i++){
-					
-					//3と5の倍数
-					if(i%15 == 0){
-						System.out.println("FizzBuzz");
-					}
-					//5の倍数
-					else if(i%5 == 0){
-						System.out.println("Fizz");
-					}
-					//3の倍数
-					else if(i%3 == 0){
-						System.out.println("Buzz");
-					}
-					//数字
-					else{
-						System.out.println(i);
-					}
-				}
 				scan.close();
 				break;
 			}
+		}
+		for (int i = 1; i <= num; i++){
+			//3と5の倍数
+			if(i%15 == 0){
+				System.out.println("FizzBuzz");
+			}
+			//3の倍数
+			else if(i%3 == 0){
+				System.out.println("Fizz");
+			}
+			//5の倍数
+			else if(i%5 == 0){
+				System.out.println("Buzz");
+			}
+			//数字
+			else{
+				System.out.println(i);
+			}
+		}
 	}
-}
 	// 数値チェック
 	static boolean chknum(String numchk) {
 		try {
